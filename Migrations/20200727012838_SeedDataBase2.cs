dc488f555e289dc7e333b22b26f1397717882c46
Jhonatan Edward <jon.edward@outlook.com>
2020-07-26 22:38:50 -0300

    Criado classe de entidade e realizada migration para SQLSERVER.

---
+++ Migrations/20200727012838_SeedDataBase2.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mvc_EFCore2.Migrations
+{
+    public partial class SeedDataBase2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Alunos(nome, sexo,email, nascimento) VALUES('Edward', 'Masculino', 'edward@dev.com', '1998-08-24')");
+            migrationBuilder.Sql("INSERT INTO Alunos(nome, sexo,email, nascimento) VALUES('Antonio', 'Masculino', 'antonio@dev.com', '1999-09-25')");
+            migrationBuilder.Sql("INSERT INTO Alunos(nome, sexo,email, nascimento) VALUES('Rosivraldo', 'Masculino', 'Rosi.valdo@dev.com', '1996-09-25')");
+            migrationBuilder.Sql("INSERT INTO Alunos(nome, sexo,email, nascimento) VALUES('Fabiana','Feminino', 'feminina@dev.com', '1990-09-25')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM alunos");
+        }
+    }
+}
---
+++ Models/Aluno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mvc_EFCore2.Models
+{
+    [Table("Alunos")]
+    public class Aluno
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Nome { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Sexo { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Email { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime Nascimento { get; set; }
+
+    }
+}
